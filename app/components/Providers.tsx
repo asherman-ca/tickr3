2deb6016b0112553d1f990175a17261d647daeff
Alex Sherman <asherman.ca@gmail.com>
2023-04-09 22:11:01 -0700

    session provider typing

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -1,6 +1,13 @@
 'use client'
 import { SessionProvider } from 'next-auth/react'
+import { sessionType } from '../utils/types'
 
-export function Providers({ children, session }: any) {
+export function Providers({
+	children,
+	session,
+}: {
+	children: React.ReactNode
+	session: sessionType
+}) {
 	return <SessionProvider session={session}>{children}</SessionProvider>
 }
