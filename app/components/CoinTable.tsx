14ee1e78f5e17aea5c1140cf31d6f984c2aa2be3
Alex Sherman <asherman.ca@gmail.com>
2023-04-16 02:21:10 -0700

    cointable styling complete

--- app/components/CoinTable.tsx
+++ app/components/CoinTable.tsx
@@ -7,6 +7,9 @@ import { getUserLikes } from '../utils/fetchers'
 import { useQuery } from '@tanstack/react-query'
 import Loader from './Loader'
 import { moneyParse, numParseTwoDecimal } from '../utils/parsers'
+import { StarIcon } from '@heroicons/react/24/outline'
+import { StarIcon as SolidStarIcon } from '@heroicons/react/24/solid'
+import Image from 'next/image'
 
 const CoinTable = ({
 	coins,
@@ -46,37 +49,63 @@ const CoinTable = ({
 		return <Loader />
 	}
 
-	// TODO: remove SSR and useQuery and just fetch raw from a useeffect so it refetches on remount to fix the like bug
-	// or try SWR
+	// TODO: get rid of react-query and if session then raw fetch likes from useEffect on every mount to fix like-change bug
+	// or see if SWR invalidation works
 
 	return (
-		<table className='w-full text-base'>
+		<table className='w-full'>
 			<thead>
-				<tr>
-					<th className='text-left pb-2'>#</th>
-					<th className='text-left'>Name</th>
-					<th className='text-right'>Price</th>
-					<th className='text-right'>1h%</th>
-					<th className='text-right'>24h%</th>
-					<th className='text-right'>7d%</th>
-					<th className='text-right'>Market Cap</th>
-					<th className='text-right'>Volume(24h)</th>
+				<tr className='border-gray-200 border-b border-t'>
+					<th className='py-2'>
+						{/* <StarIcon height={14} width={14} className='' /> */}
+					</th>
+					<th className='text-left py-4'>#</th>
+					<th className='text-left '>Name</th>
+					<th className='text-right '>Price</th>
+					<th className='text-right '>1h%</th>
+					<th className='text-right '>24h%</th>
+					<th className='text-right '>7d%</th>
+					<th className='text-right '>Market Cap</th>
+					<th className='text-right pr-2'>Volume(24h)</th>
 				</tr>
 			</thead>
 			<tbody>
 				{displayCoins.map((coin) => {
 					return (
-						<tr key={coin.id} className='hover:bg-blue-50'>
-							<td className='text-left py-2'>{coin.market_cap_rank}</td>
+						<tr
+							key={coin.id}
+							className='hover:bg-blue-50 border-b border-gray-200'
+						>
+							<td className='pl-2'>
+								{coin.liked ? (
+									<SolidStarIcon height={14} width={14} color={'gold'} />
+								) : (
+									<StarIcon height={14} width={14} />
+								)}
+							</td>
+							<td className='text-left py-6'>{coin.market_cap_rank}</td>
 							<td>
 								<Link
 									href={`/coin/${coin.id}`}
-									className={`${coin.liked && 'text-red-500'}`}
+									className='font-medium flex items-center gap-1'
 								>
-									{coin.name}
+									<Image
+										src={coin.image}
+										height={24}
+										width={24}
+										alt={coin.name}
+									/>
+									<div>
+										{coin.name}{' '}
+										<span className='text-slate-500 font-normal'>
+											{coin.symbol.toUpperCase()}
+										</span>
+									</div>
 								</Link>
 							</td>
-							<td className='text-right'>{moneyParse(coin.current_price)}</td>
+							<td className='text-right font-medium'>
+								{moneyParse(coin.current_price)}
+							</td>
 							<td
 								className={`text-right ${
 									coin.price_change_percentage_1h_in_currency > 0
@@ -114,7 +143,9 @@ const CoinTable = ({
 								%
 							</td>
 							<td className='text-right'>{moneyParse(coin.market_cap)}</td>
-							<td className='text-right'>{moneyParse(coin.total_volume)}</td>
+							<td className='text-right pr-2'>
+								{moneyParse(coin.total_volume)}
+							</td>
 						</tr>
 					)
 				})}
