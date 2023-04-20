e5535adf58e7258650a569082484b7dff6ef00b1
Alex Sherman <asherman.ca@gmail.com>
2023-04-19 21:29:22 -0700

    profile buttons

---
+++ app/profile/components/PortfolioTable.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const PortfolioTable = () => {
+	return <div>PortfolioTable</div>
+}
+
+export default PortfolioTable
--- app/profile/components/ProfileTable.tsx
+++ app/profile/components/ProfileTable.tsx
@@ -1,7 +1,33 @@
+'use client'
 import { useState } from 'react'
+import { ChartPieIcon, StarIcon } from '@heroicons/react/24/solid'
+import WatchListTable from './WatchListTable'
+import PortfolioTable from './PortfolioTable'
 
 const ProfileTable = ({ user }: any) => {
-	return <div>ProfileTable</div>
+	const [showWatchlist, setShowWatchlist] = useState(true)
+
+	return (
+		<div className='flex flex-col gap-4 px-12'>
+			<div className='flex gap-4'>
+				<button
+					className='p-2 bg-gray-100 rounded-md flex items-cente gap-2 profile-button'
+					onClick={() => setShowWatchlist(true)}
+				>
+					<StarIcon height={18} width={18} color={'gray'} />
+					Watchlist
+				</button>
+				<button
+					className='p-2 bg-gray-100 rounded-md flex items-center gap-2 profile-button'
+					onClick={() => setShowWatchlist(false)}
+				>
+					<ChartPieIcon height={18} width={18} color={'gray'} />
+					Portfolio
+				</button>
+			</div>
+			{showWatchlist ? <WatchListTable /> : <PortfolioTable />}
+		</div>
+	)
 }
 
 export default ProfileTable
