7e15bd52092b7ea3e220bfd8702efe34e1980407
Alex Sherman <asherman.ca@gmail.com>
2023-04-19 21:34:13 -0700

    leaderboard placeholder

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { WrenchScrewdriverIcon } from '@heroicons/react/24/outline'
 
 function page() {
-	return <div>leaderboard</div>
+	return (
+		<div className='flex flex-1 justify-center items-center text-2xl font-bold gap-2'>
+			<WrenchScrewdriverIcon height={48} width={48} color={'black'} />
+			Under Construction
+		</div>
+	)
 }
 
 export default page
